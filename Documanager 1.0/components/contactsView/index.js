8d341526e6da4a2c972ed07214cc39ce7d25842d
Jason Romero <codenamejason@outlook.com>
2016-05-28 22:07:52 +0000

    Changes made by Jason Romero (codenamejason@outlook.com).

--- Documanager 1.0/components/contactsView/index.js
+++ Documanager 1.0/components/contactsView/index.js
@@ -8,13 +8,14 @@ app.contactsView = kendo.observable({
 // START_CUSTOM_CODE_contactsView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function () {
-    app.contactsView.set('title', 'Contacts');
+    app.contactsView.set('title', 'Contact Us');
     
-    var dataSource = new kendo.data.DataSource({
-        data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
+    var empdataSource = new kendo.data.DataSource({
+        data: [{ id: 1, name: 'Jason Romero', email: 'codenamejason@outlook.com', phone: '727-260-0629' }, { id: 2, name: 'Jennifer Teverini', email: 'jplylldy@outlook.com', phone: '727-512-0155' }]
     });
-    app.contactsView.set('dataSource', dataSource);
-    
-    app.contactsView.set('alert', function (e) { alert(e.data.name); });
+  	// Set data source for Contact Us page
+    app.contactsView.set('dataSource', empdataSource);
+    // Set the alert for the Contact Us View
+    app.contactsView.set('alert', function (e) { alert(e.data.name + ' ' + e.data.email + ' ' + e.data.phone); });
 })();
 // END_CUSTOM_CODE_contactsView
--- Documanager 1.0/components/privacyView/index.js
+++ Documanager 1.0/components/privacyView/index.js
@@ -8,6 +8,6 @@ app.privacyView = kendo.observable({
 // START_CUSTOM_CODE_homeView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function () {
-    app.privacyView.set('title', 'Privacy Policy');
+    app.privacyView.set('title', 'Home');
 })();
 // END_CUSTOM_CODE_homeView
