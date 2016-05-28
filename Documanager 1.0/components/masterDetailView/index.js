89b252c869dcac3b46e0cb305d8cb1edd7f52ac5
Jason Romero <codenamejason@outlook.com>
2016-05-28 21:23:14 +0000

    Changes made by Jason Romero (codenamejason@outlook.com).

---
+++ Documanager 1.0/components/masterDetailView/index.js
@@ -0,0 +1,133 @@
+'use strict';
+
+app.masterDetailView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_masterDetailView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_masterDetailView
+(function(parent) {
+    var dataProvider = app.data.documanager10,
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('masterDetailViewModel'),
+                dataSource = model.get('dataSource');
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'dbo_Customers',
+                dataProvider: dataProvider
+            },
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            error: function(e) {
+                if (e.xhr) {
+                    alert(JSON.stringify(e.xhr));
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'ContactName': {
+                            field: 'ContactName',
+                            defaultValue: ''
+                        },
+                        'Phone': {
+                            field: 'Phone',
+                            defaultValue: ''
+                        },
+                    },
+                    icon: function() {
+                        var i = 'globe';
+                        return kendo.format('km-icon km-{0}', i);
+                    }
+                }
+            },
+            serverFiltering: true,
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        masterDetailViewModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+
+                app.mobileApp.navigate('#components/masterDetailView/details.html?uid=' + e.dataItem.uid);
+
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = masterDetailViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+
+                if (!itemModel.Id) {
+                    itemModel.Id = String.fromCharCode(160);
+                }
+
+                masterDetailViewModel.set('currentItem', null);
+                masterDetailViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('masterDetailViewModel', masterDetailViewModel);
+        });
+    } else {
+        parent.set('masterDetailViewModel', masterDetailViewModel);
+    }
+
+    parent.set('onShow', function(e) {
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null;
+
+        fetchFilteredData(param);
+    });
+})(app.masterDetailView);
+
+// START_CUSTOM_CODE_masterDetailViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_masterDetailViewModel
